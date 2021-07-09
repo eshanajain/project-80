60769e520aef634753e759793bcb951351758e21
eshanajain <82397896+eshanajain@users.noreply.github.com>
2021-07-09 17:42:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var GuestName = document.getElementById("name1").value;
+name_of_people.push(GuestName);
+
+name_of_people.sort
+
+function searching(){
+     var s= document.getElementById("s1").value;
+     var found=0;
+     var j;
+     for(j=0; j<name_of_people.length; j++)
+     {
+         if(s==names_of_people[j]){
+
+         }
+     }
+     document.getElementById("p2").innerHTML="name found "+found+"times"
+     console.logfound name("")
+}
